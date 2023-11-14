ff69bb46fa938b9107f98db631fad2f3ca282739
konstantin-ufimtsev <k.ufimcev@yandex.ru>
2023-11-14 06:51:05 +0200

    добавлен модуль сичтывания excel

---
+++ input_xlsx.py
@@ -0,0 +1,18 @@
+import openpyxl
+
+book = openpyxl.open('input.xlsx', read_only=True)
+sheet = book.active
+
+sheet_data = []
+
+for row in range(2, sheet.max_row + 1):
+    city: str = sheet[row][0].value
+    bau_link: str = sheet[row][1].value
+    leroy_link: str = sheet[row][2].value
+    conv_factor: bool = sheet[row][3].value
+    retail_price: bool = sheet[row][4].value
+    tuple_row = (city, bau_link, leroy_link, conv_factor, retail_price) #кортеж значений листа
+    sheet_data.append(tuple_row)
+
+
+
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import random
 
 
 
+
+
 #logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
 #    datefmt='[%d.%m.%Y-%H:%M:%S]',
 #    level=logging.DEBUG,
